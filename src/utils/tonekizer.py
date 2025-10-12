bac49648db97aca58b316174ca41ff1c481cb5b6
LeviJunior21 <levi.pereira.junior@ccc.ufcg.edu.br>
2025-10-12 14:20:14 -0300

    add: adicionando modelo

---
+++ src/utils/tonekizer.py
@@ -0,0 +1,13 @@
+import torch, tiktoken
+
+tokenizer = tiktoken.get_encoding("o200k_base")
+
+def text_to_token_ids(text, tokenizer, device="cpu"):
+    encoded = tokenizer.encode(text, allowed_special={'<|endoftext|>'})
+    encoded_tensor = torch.tensor(encoded).unsqueeze(0)
+    return encoded_tensor.to(device)
+
+
+def token_ids_to_text(token_ids, tokenizer, device="cpu"):
+    flat = token_ids.squeeze(0)
+    return tokenizer.decode(flat.tolist())
